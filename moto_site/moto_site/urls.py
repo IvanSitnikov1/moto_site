e3fe88a99614f868238daaeac8b0471df1a79c32
ivan <iw.sitnikoff@yandex.ru>
2023-08-06 18:05:34 +0300

    create models Motorcycle and Category

--- moto_site/moto_site/urls.py
+++ moto_site/moto_site/urls.py
@@ -16,12 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
+from moto_site import settings
 from main.views import pageNotFound
 
 urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = pageNotFound
